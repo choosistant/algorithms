277e5a663e8b46ceedb97629ef16eef9480d4843
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-10-05 06:17:08 +0200

    Fix runtime error.

--- src/data.py
+++ src/data.py
@@ -137,7 +137,7 @@ class AmazonReviewQADataModule(pl.LightningDataModule):
 
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
-            dataset=self._get_data_set(),
+            dataset=self.get_data_set(),
             batch_size=self._batch_size,
         )
 
